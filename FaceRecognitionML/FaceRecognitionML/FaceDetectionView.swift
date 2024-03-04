0c0340e0314dc6287c3a5f4a6684e018c63a0434
Jason <sanchezjason@me.com>
2024-03-04 16:10:22 -0700

    swiftui/ML_face_recognition- classificationLabel

--- FaceRecognitionML/FaceRecognitionML/FaceDetectionView.swift
+++ FaceRecognitionML/FaceRecognitionML/FaceDetectionView.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import Vision
 
 struct FaceDetectionView: View {
     
     let photos = ["face","friends-sitting","people-sitting","ball","bird"]
     
     @State private var currentIndex: Int = 0
+    @State private var classificationLabel: String = ""
+    
+    private func detectFaces(completion: @escaping ([VNFaceObservation]?) -> Void) {
+        guard let image = UIImage(named: photos[currentIndex]),
+              let cgImage = image.cgImage,
+              let orientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue)) else {
+            return completion(nil)
+        }
+        let request = VNDetectFaceRectanglesRequest()
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage,
+                                            orientation: orientation,
+                                            options: [:])
+        DispatchQueue.global().async {
+            try? handler.perform([request])
+            
+            guard let observations = request.results else {
+                return completion(nil)
+            }
+            completion(observations)
+        }
+    }
     
     var body: some View {
         VStack {
@@ -46,19 +69,26 @@ struct FaceDetectionView: View {
                 .background(Color.gray)
                 .cornerRadius(10)
             
-                
-                
             }.padding()
             
             Button("Classify") {
                 // classify the image here
+                self.detectFaces { results in
+                    if let results = results {
+                        // update the UI
+                        DispatchQueue.main.async {
+                            self.classificationLabel = "Faces: \(results.count)"
+                            print(classificationLabel)
+                        }
+                    }
+                }
                 
             }.padding()
             .foregroundColor(Color.white)
             .background(Color.green)
             .cornerRadius(8)
             
-            Text("")
+            Text(classificationLabel)
         }
     }
 }
