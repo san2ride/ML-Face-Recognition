b14b58e39cc958d43507d04f4b601e7f69f8b53d
Jason <sanchezjason@me.com>
2024-03-04 15:17:20 -0700

    swiftui/ML_face_recognition- Project setup

---
+++ FaceRecognitionML/FaceRecognitionML/FaceRecognitionMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionMLApp.swift
+//  FaceRecognitionML
+//
+//  Created by Cause  I'm Electric on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecognitionMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
